da6ca6285712a86f85cd45950e825ce3ac348142
partheinstein@gmail.com <partheinstein@gmail.com@4f3449ad-8247-f4b8-181e-5347b17ea172>
2012-04-02 08:19:35 +0000

    Added class file and properties file uploading capabilities for algorithms

--- trunk/ repsystestbed/RepSysTestbedWeb/src/cu/rst/gwt/client/AlgAddPPanel.java
+++ trunk/ repsystestbed/RepSysTestbedWeb/src/cu/rst/gwt/client/AlgAddPPanel.java
@@ -1,9 +1,14 @@
 package cu.rst.gwt.client;
 
-import java.util.ArrayList;
-
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.FileUpload;
@@ -12,47 +17,83 @@ import com.google.gwt.user.client.ui.FormPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class AlgAddPPanel extends PopupPanel 
+public final class AlgAddPPanel extends FormPanel 
 {
-	FormPanel form;
-	Label nameL;
-	TextBox nameTB;
-	Label classL;
-	Label propL;
-	Button okB;
-	FlexTable flexT;
+	final Label nameL;
+	final TextBox nameTB;
+	final Label classL;
+	final Label propL;
+	final Button okB;
+	final FlexTable flexT;
+	final FileUpload classUpload;
+	final FileUpload propUpload;
 	//TODO move this to server
 	
 	ListBox graphsLB;
 	CheckBox evalAlgCB;
 	
+	public class AlgAddClickHandler implements ClickHandler
+	{
+		private AlgAddPPanel panel;
+		public AlgAddClickHandler(AlgAddPPanel panel)
+		{
+			setPanel(panel);
+		}
+		public AlgAddPPanel getPanel() {
+			return panel;
+		}
+		public void setPanel(AlgAddPPanel panel) {
+			this.panel = panel;
+		}
+		
+		@Override
+		public void onClick(ClickEvent event) 
+		{
+			panel.submit();
+			panel.setVisible(false);
+			panel.reset();
+		}
+	}
+	
+
 	public AlgAddPPanel(final FlexTable flexT)
 	{
 		//click outside to close window
-		super(true);
+		super();
 		this.flexT = flexT;
-		form = new FormPanel();
-	    form.setAction("/myFormHandler");
+		
+		//TODO - create static constants for the op codes 
+	    super.setAction(GWT.getModuleBaseURL() + "newalg");
+	    super.setEncoding(FormPanel.ENCODING_MULTIPART);
+	    super.setMethod(FormPanel.METHOD_POST);
+	    
 		nameL = new Label("Algorithm name:");
 		nameTB = new TextBox();
+		nameTB.setName("nameFormElement");
+		
+		
 		classL = new Label("Java class location:");
 		propL = new Label("Properties file location:");
+		
 		okB = new Button("OK");
+		
 		evalAlgCB = new CheckBox("Evaluation Algorithm");
+		
 		graphsLB = new ListBox();
 		graphsLB.setVisible(false);
 		
-		FileUpload classUpload = new FileUpload();
-		FileUpload propUpload = new FileUpload();
+		classUpload = new FileUpload();
+		propUpload = new FileUpload();
 		classUpload.setName("classUploadFormElement");
 		propUpload.setName("propUploadFormElement");
 		
+		
 		VerticalPanel holder = new VerticalPanel();
 		HorizontalPanel hPanel = new HorizontalPanel();
+		
 		hPanel.add(evalAlgCB);
 		hPanel.add(graphsLB);
 		holder.add(nameL);
@@ -63,20 +104,11 @@ public class AlgAddPPanel extends PopupPanel
 		holder.add(propUpload);
 		holder.add(hPanel);
 		holder.add(okB);
-		add(holder);
 		
-		okB.addClickHandler(new ClickHandler()
-		{
-
-			public void onClick(ClickEvent event) 
-			{
-				final String algName = nameTB.getText().toUpperCase().trim();
-				addAlg(algName);
-				nameTB.setText("");				
-				hide();			
-			}
-			
-		});
+		super.setWidget(holder);
+		//add(form);
+		
+		okB.addClickHandler(new AlgAddClickHandler(this));
 		
 		evalAlgCB.addClickHandler(new ClickHandler()
 		{
@@ -99,6 +131,30 @@ public class AlgAddPPanel extends PopupPanel
 			
 			
 		});
+		
+		super.addSubmitHandler(new FormPanel.SubmitHandler() 
+		{
+			
+			@Override
+			public void onSubmit(SubmitEvent event) 
+			{
+				final String algName = nameTB.getText().toUpperCase().trim();
+				addAlg(algName);
+				nameTB.setText("");
+			}
+		});
+		
+		super.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler()
+		{
+			
+			@Override
+			public void onSubmitComplete(SubmitCompleteEvent event) 
+			{
+				 Window.alert(event.getResults());
+				
+			}
+		});
+
 
 	}
 	
@@ -114,31 +170,89 @@ public class AlgAddPPanel extends PopupPanel
 		return false;
 	}
 	
+//	public void addAlg(final String name)
+//	{
+//		if(!TestbedUI.algTable.contains(name) && !name.equals(""))
+//		{
+//			TestbedUI.algTable.add(name);
+//			int row = flexT.insertRow(flexT.getRowCount());
+//			flexT.setText(row, 0, name);
+//			Button removeB = new Button("x");
+//			flexT.setWidget(row, 1, removeB);
+//			
+//			removeB.addClickHandler((new ClickHandler()
+//			{
+//				public void onClick(ClickEvent event) 
+//				{
+//					int removeIndex = TestbedUI.algTable.indexOf(name);
+//					if(removeIndex >= 0)
+//					{
+//						TestbedUI.algTable.remove(removeIndex);
+//						flexT.removeRow(removeIndex + 1); //don't remove the table header
+//					}
+//				}
+//				
+//			}));
+//
+//		}
+//	}
+	
 	public void addAlg(final String name)
 	{
-		if(!TestbedUI.algTable.contains(name) && !name.equals(""))
+		int row = flexT.insertRow(flexT.getRowCount());
+		flexT.setText(row, 0, name);
+		Button removeB = new Button("x");
+		flexT.setWidget(row, 1, removeB);
+		removeB.addClickHandler(new ClickHandler()
 		{
-			TestbedUI.algTable.add(name);
-			int row = flexT.insertRow(flexT.getRowCount());
-			flexT.setText(row, 0, name);
-			Button removeB = new Button("x");
-			flexT.setWidget(row, 1, removeB);
-			
-			removeB.addClickHandler((new ClickHandler()
+			public void onClick(ClickEvent event) 
 			{
-				public void onClick(ClickEvent event) 
-				{
-					int removeIndex = TestbedUI.algTable.indexOf(name);
-					if(removeIndex >= 0)
-					{
-						TestbedUI.algTable.remove(removeIndex);
-						flexT.removeRow(removeIndex + 1); //don't remove the table header
-					}
-				}
+				//int removeIndex = TestbedUI.algTable.indexOf(name);
 				
-			}));
+				String url = TestbedUI.JSON_URL;
+				url = url + "op=rem_alg" + "&alg_name=" + name;
+				// Send request to server and catch any errors.
+			    RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, url);
+			    try 
+			    {
+			    	Request request = builder.sendRequest(null, new RequestCallback()
+			    	 {
 
-		}
+						@Override
+						public void onResponseReceived(Request request, Response response) 
+						{
+							if(response.getStatusCode() == 200)
+							{
+								int removeIndex = -1;
+								try
+								{
+									String t = response.getText();
+									removeIndex = Integer.parseInt(t);
+								}
+								catch(Exception e)
+								{
+									Window.alert(e.toString());
+								}
+								if(removeIndex >= 0) flexT.removeRow(removeIndex + 1);
+							}
+							
+						}
+
+						@Override
+						public void onError(Request request, Throwable exception) 
+						{
+
+							
+						}
+			    		 
+			    	 });
+			    }
+			    catch(RequestException e)
+			    {
+
+			    }
+			}
+		});
 	}
 	
 }
--- trunk/ repsystestbed/RepSysTestbedWeb/src/cu/rst/gwt/server/RSTServlet.java
+++ trunk/ repsystestbed/RepSysTestbedWeb/src/cu/rst/gwt/server/RSTServlet.java
@@ -1,27 +1,183 @@
 package cu.rst.gwt.server;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
 import com.google.gson.Gson;
+import com.google.gwt.core.client.GWT;
 
 import cu.rst.gwt.client.Algorithm;
 
 public class RSTServlet extends HttpServlet 
 {
+	
+	//Algorithm class is used for sendings json structures but the class and properties bytes are
+	//are never sent back to the client
 	Hashtable<String, Algorithm> algs = new Hashtable<String, Algorithm>();
+	Hashtable<String, byte[]> algClasses = new Hashtable<String, byte[]>();
+	Hashtable<String, byte[]> propClasses = new Hashtable<String, byte[]>();
 	
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
 	{
-		algs.put("bla", new Algorithm("bla"));
+		String op = req.getParameter("op");
+		if(op.toLowerCase().trim().equals("get_algs"))
+		{
+			processGetAlgs(req, resp);
+		}
+		else if(op.toLowerCase().trim().equals("rem_alg"))
+		{
+			processRemoveAlg(req, resp);
+		}
+		
+	}
+	
+	private void processRemoveAlg(HttpServletRequest req, HttpServletResponse resp)  throws ServletException, IOException 
+	{
+		String algName = req.getParameter("alg_name");
+		
+		int pos = 0;
+		int i = 0;
+		for(String n : algs.keySet())
+		{
+			if(n.equals(algName))
+			{
+				pos = i;
+				break;
+			}
+			i++;
+		}
+		
+		algs.remove(algName);
+		algClasses.remove(algName);
+		propClasses.remove(algName);
+		
+		PrintWriter out = resp.getWriter();
+		out.print(pos);
+		out.flush();
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+//		String op = req.getParameter("op");
+//		if(op.toLowerCase().trim().equals("putalgs"))
+//		{
+			processPutAlgs(req, resp);
+//		}
+			
+	}
+	
+	private void processPutAlgs(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
+		try 
+		{
+			ServletFileUpload upload = new ServletFileUpload();
+			String algName = null;
+			byte[] classBytes = null;
+			byte[] propBytes = null;
+
+			FileItemIterator iterator = upload.getItemIterator(req);
+			while (iterator.hasNext()) 
+			{
+				FileItemStream item = iterator.next();
+				InputStream stream = item.openStream();
+				//not a file (e.g text field)
+				if (item.isFormField() && item.getFieldName().equals("nameFormElement")) 
+				{  
+					StringWriter writer = new StringWriter();
+					IOUtils.copy(stream, writer);
+					algName = writer.toString();
+
+				}
+				else 
+				{
+					if(item.getFieldName().equals("classUploadFormElement"))
+					{
+						ByteArrayOutputStream out = new ByteArrayOutputStream();
+						int len;
+						byte[] buffer = new byte[8192];
+	    	 			while ((len = stream.read(buffer, 0, buffer.length)) != -1)
+	    	 			{
+	    	 				out.write(buffer, 0, len);
+	    	 			}
+	    	 			classBytes = out.toByteArray();
+					}
+					else if(item.getFieldName().equals("propUploadFormElement"))
+					{
+						ByteArrayOutputStream out = new ByteArrayOutputStream();
+						int len;
+						byte[] buffer = new byte[8192];
+	    	 			while ((len = stream.read(buffer, 0, buffer.length)) != -1)
+	    	 			{
+	    	 				out.write(buffer, 0, len);
+	    	 			}
+	    	 			propBytes = out.toByteArray();
+					}
+
+
+				}
+			}
+			
+			if(algName == null)
+			{
+				throw new Exception("No alg name");
+			}
+			
+			//don't add if its already there.
+			if(!this.algs.containsKey(algName))
+			{
+				this.algs.put(algName, new Algorithm(algName));
+				if(classBytes != null && classBytes.length != 0)
+				{
+					this.algClasses.put(algName, classBytes);
+					if(propBytes != null && propBytes.length != 0)
+					{
+						this.propClasses.put(algName, propBytes);
+					}
+				}
+			}
+			
+		      
+		}
+		catch (Exception ex) 
+		{
+			throw new ServletException(ex);
+		}
+
+    }
+		
+	
+
+	private void processGetAlgs(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+	{
 		PrintWriter out = resp.getWriter();
 		Gson gson = new Gson();
 		out.println('[');
@@ -34,3 +190,4 @@ public class RSTServlet extends HttpServlet
 		out.flush();
 	}
 }
+
